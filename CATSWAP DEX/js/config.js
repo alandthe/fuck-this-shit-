151f0e0cd24b8db151dfc4d5ad27b3105344b0e5
alandthe <topeditz.business@gmail.com>
2025-04-09 21:59:16 +0300

    Create config.js

---
+++ CATSWAP DEX/js/config.js
@@ -0,0 +1,20 @@
+// Chain Configuration
+export const SONIC_CHAIN = {
+  id: 146,
+  name: "Sonic",
+  rpcUrl: "https://rpc.sonic.network",
+  explorer: "https://sonicscan.io",
+  currencySymbol: "SONIC"
+};
+
+// Contract Addresses
+export const CONTRACT_ADDRESSES = {
+  dex: "0x00c8e7be475002587cf6880b096bedbb60d19ef6",
+  wcToken: "0xDD175A3998D81C3Ef51aBB9c1Eab2D7a8C795F68",
+  sonicToken: "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38"
+};
+
+// WalletConnect
+export const WALLETCONNECT_CONFIG = {
+  projectId: "1d7b16153de36a7ee42b7f5c64458846"
+};
